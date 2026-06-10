86d4117df3fbcae2261d348cf560220a28b93665
owais74474-glitch <owais.74474@gmail.com>
2026-06-10 13:20:47 +0500

    Add Student class with inheritance

---
+++ src/Person/student.h
@@ -0,0 +1,31 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include "Person.h"
+class Student:public Person
+{
+    protected:
+    string rollNo;
+    int semester;
+    float gpa;
+
+    static const int MAX_COURSES=10;
+    string enrolledCourses[MAX_COURSES];
+    int courseCount;
+
+    public:
+    Student();
+    Student(string name,string cnic,int age,string contact,string rollNo,int semester,float gpa);
+virtual ~Student();
+string getRollNo() const;
+int getSemester() const;
+float getGPA() const;
+ 
+void setSemester(int semester);
+void setGPA(float gpa);
+
+void addCourse(string courseName);
+char calculateGrade() const;
+
+void displayInfo() const override;
+};
+#endif
