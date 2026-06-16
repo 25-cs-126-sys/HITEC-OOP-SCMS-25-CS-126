9596cf05dbe8bee6035163310210a6686968752f
owais74474-glitch <owais.74474@gmail.com>
2026-06-16 22:57:59 +0500

    update

--- src/person/person.h
+++ src/person/person.h
@@ -1,22 +1,26 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
 using namespace std;
-class Person{
-    protected:
+class Person
+{
+protected:
     string name;
     string cnic;
     int age;
     string contact;
-    public:
-    Person(string name,string cnic,int age,string contact);
-    virtual ~Person();
-    string getName() const;
-    string getCNIC() const;
-    int getAge() const;
-    string getContact() const;
-    virtual void displayInfo() const=0;
+public:
+    Person(string n, string c, int a, string con)
+    {
+        name = n;
+        cnic = c;
+        age = a;
+        contact = con;
+    }
+    virtual void displayInfo() const = 0;
+    virtual ~Person() {}
 };
-#endif
+
+ #endif
